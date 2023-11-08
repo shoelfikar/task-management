f85722b6014633a850e8d6c6ad1158520e3d2c7c
shoelfikar <sulfikardi@wgroup.id>
2023-11-08 13:38:06 +0700

    init nestjs project

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { Task } from './tasks.model';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTask(): Task[] {
+        return this.tasks;
+    }
+}
