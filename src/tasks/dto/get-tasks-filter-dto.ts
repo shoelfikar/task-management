017289bac2eef2e1344fff5ffc93eef948436afb
shoelfikar <sulfikardi@wgroup.id>
2023-11-15 14:14:42 +0700

    add validate filter task

--- src/tasks/dto/get-tasks-filter-dto.ts
+++ src/tasks/dto/get-tasks-filter-dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../tasks.model";
 
 export class FilterTaskDto {
+    @IsOptional()
+    @IsEnum(TaskStatus)
     search?:string;
+
+    @IsOptional()
+    @IsString()
     status?: TaskStatus
 }
